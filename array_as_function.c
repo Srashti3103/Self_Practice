b5ce9d0c2dd2bb45e2015e1f3b99d29c29331f3b
Srashti3103 <srashtipatel31@gmail.com>
2025-10-27 19:00:24 +0530

    print number

---
+++ array_as_function.c
@@ -0,0 +1,30 @@
+//array as function 
+// *arr and arr[] gives same output 
+// #include<stdio.h>
+// void printnumbers( int arr[], int n);
+// int main()
+// {
+//     int arr[]= {1,2,3,4,5,6};
+//     printnumbers(arr,6);
+//     return 0;
+// }
+//  void printnumbers(int arr[], int n){
+//     for (int i=0; i<n; i++){
+//         printf("%d\t", arr[i]);
+//     }
+//     printf("\n");
+//  }
+#include<stdio.h>
+void printnumbers( int *arr, int n);
+int main()
+{
+    int arr[]= {1,2,3,4,5,6};
+    printnumbers(arr,6);
+    return 0;
+}
+ void printnumbers(int *arr, int n){
+    for (int i=0; i<n; i++){
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+ }
