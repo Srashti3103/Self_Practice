3c04c49d45c53b0225c0a6887887c0dbfcca7cb3
Srashti3103 <srashtipatel31@gmail.com>
2025-10-31 21:30:03 +0530

    function

---
+++ function.c
@@ -0,0 +1,15 @@
+//function basic
+#include<stdio.h>
+//dedclaration / prototype
+void printhello();
+int main()
+{
+  //calling of function
+  printhello(); 
+  printhello();
+  return 0; 
+}
+//function definition
+void printhello(){
+    printf("hello world\n");
+}
