f43a8ac6603dbbcd1df3fa185684e5cb617536ac
Srashti3103 <srashtipatel31@gmail.com>
2025-10-31 21:28:43 +0530

    function

---
+++ function_3.c
@@ -0,0 +1,22 @@
+//print namaste if user is indian and print bonjour if  user is french
+#include<stdio.h>
+void indian();
+void french();
+int main()
+{
+    char ch;
+    printf("Enter i for indian and f for french :\n");
+    scanf("%c",&ch);
+    if(ch == 'i'){
+        indian();
+    }else if (ch == 'f'){
+        french();
+    }
+    return 0;
+}
+void indian(){
+    printf("NAMASTE\n");
+}
+void french(){
+    printf("BONJOUR\n");
+}
