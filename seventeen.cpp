dd9f605e037083f48548140c55a12d02df50a37c
Srashti3103 <srashtipatel31@gmail.com>
2025-11-01 13:39:16 +0530

    seventeen

---
+++ seventeen.cpp
@@ -0,0 +1,16 @@
+ // even odd
+ #include<stdio.h>
+ int main()
+ {
+ 	int a;
+ 	printf("ENter a number :");
+ 	scanf("%d",&a);
+ 	if(a%2==0)
+ 	{
+ 		printf("number is even ");
+	 }
+	 else 
+	 {
+	 	printf("number is odd `");
+	 }
+}
