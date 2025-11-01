9d79ee442c3e892f1552d144b7af01f2d31af035
Srashti3103 <srashtipatel31@gmail.com>
2025-11-02 00:54:22 +0530

    string

---
+++ string.c
@@ -0,0 +1,30 @@
+// //create a string firstname  &  lastname to store details of user & print all the characters using  a loop 
+ #include<stdio.h>
+// void printstring(char arr[]);
+// int main()
+// {
+//         char firstname[] = "SRASHTI";
+//         char lastname[] = "PATEL";
+//         printstring(firstname);
+//         printstring(lastname);
+//         return 0;
+// }
+// void printstring(char arr[]){
+//     for(int i=0 ; arr[i]!='\0'; i++){
+//         printf("%c",arr[i]);
+//     }
+//     printf("\n");
+// }
+
+
+// int main(){
+//     char name[] = "SRASHTI";
+//     printf("%s",name);
+// }
+  int main()
+  {
+    char name[50];
+    scanf("%s",name);
+    printf("your name : %s",name);
+    return 0;
+  }
