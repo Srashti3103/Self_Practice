27bb7b0f514dd2486961665d483f2bccb664ada4
Srashti3103 <srashtipatel31@gmail.com>
2025-11-02 00:57:30 +0530

    staircase pattern

---
+++ staircase_pattern_3.c
@@ -0,0 +1,15 @@
+//staircase pattern
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(int i=n;i>0;i--){
+        int k=(i%2!=0)?i+1:i;
+        for(int j=0;j<k;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
