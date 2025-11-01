90e47e1cf18ae2df98cca0e55e4f7db3d5f04af8
Srashti3103 <srashtipatel31@gmail.com>
2025-11-02 00:55:10 +0530

    string

---
+++ string2.c
@@ -0,0 +1,10 @@
+//Ask the user to enter their first name  & print it back to them 
+//also try this with their fullname
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    gets(str);
+    puts(str);
+    return 0;
+}
