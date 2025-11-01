3f58efe2909e94628ed7521127a572209e400d70
Srashti3103 <srashtipatel31@gmail.com>
2025-11-01 13:38:33 +0530

    second

---
+++ second.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a ,b;
+    printf("enter a value of a");
+    scanf("%d",&a);
+    printf( "enter a value of b");
+    scanf("%d",&b);
+    printf("sum of a and b :%d",a+b);
+    return 0 ;
+}
