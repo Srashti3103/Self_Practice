0f06289da07f6d9dde7f83e01bc3877951efb33f
Srashti3103 <srashtipatel31@gmail.com>
2025-11-03 20:07:44 +0530

    twentyeight

---
+++ twentyeight.c
@@ -0,0 +1,18 @@
+//ho;;ow rectangle
+#include<stdio.h>
+int main()
+{
+    int n=6;
+    int m=28;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m ; j++){
+            if((i==0 || i==n-1)&&j<m || 
+                i<n && j==0 || j==m-1)
+                printf("*");
+                else{
+                    printf(" ");
+                }
+        }
+        printf("\n");
+    }
+}
