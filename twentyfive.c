84887cdb988cb54a0cbc23e2e3501313906f9544
Srashti3103 <srashtipatel31@gmail.com>
2025-11-03 20:08:16 +0530

    twenty five

---
+++ twentyfive.c
@@ -0,0 +1,30 @@
+// C program to print alphabet A pattern
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of m :");
+    scanf("%d",&n);
+
+    // Outer for loop for number of lines
+    for (int i = 0; i < n; i++) {
+
+        // Inner for loop for logic execution
+        for (int j = 0; j <= n / 2; j++) {
+
+            // prints two column lines
+            if ((j == 0 || j == n / 2) && i != 0 ||
+
+                // print first line of alphabet
+                i == 0 && j != 0 && j != n / 2 ||
+
+                // prints middle line
+                i == n / 2)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+}
