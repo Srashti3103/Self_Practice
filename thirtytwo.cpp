c885248d306fac5231cd9a4dba74112eaedf8595
Srashti3103 <srashtipatel31@gmail.com>
2025-11-04 23:39:11 +0530

    thirtytwo

---
+++ thirtytwo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using  namespace std;
+
+class Animal 
+
+class Mammal : public Animal 
+{
+	public : 
+	void walk ()
+	{
+		cout << "This animal walk on land ."<< endl;
+	}
+};
+int main() {
+ d_og d;
+ d.eat();
+ d.walk();
+ d.bark();
+ return 0;
+}
